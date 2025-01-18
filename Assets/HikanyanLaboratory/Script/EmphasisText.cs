5974d447a523794bf47b6542e6aef366f12b6c8e
Hikanyan <hikanyan0131@gmail.com>
2025-01-18 09:49:08 +0900

    All

--- Assets/HikanyanLaboratory/Script/EmphasisText.cs
+++ Assets/HikanyanLaboratory/Script/EmphasisText.cs
@@ -1,7 +1,11 @@
+using UnityEngine;
+
 namespace HikanyanLaboratory.Script
 {
+    [System.Serializable]
     public struct EmphasisText
     {
-        
+        public string _targetText;
+        public Color _color;
     }
 }
--- Assets/HikanyanLaboratory/Script/MessageSequencer.cs
+++ Assets/HikanyanLaboratory/Script/MessageSequencer.cs
@@ -1,7 +1,73 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
 namespace HikanyanLaboratory.Script
 {
-    public class MessageSequencer
+
+
+    public class MessageSequencer : MonoBehaviour
     {
-        
+        [SerializeField] private MessagePrinter _messagePrinter = default;
+        [SerializeField] private string[] _messages = default;
+        [SerializeField] private List<EmphasisText> _emphasisTexts = default;
+        private int _currentIndex = -1;
+
+
+        private void Start()
+        {
+            MoveNext();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (_messagePrinter != null && _messagePrinter.IsPrinting)
+                {
+                    _messagePrinter.Skip();
+                }
+                else
+                {
+                    MoveNext();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 次のページに進む。
+        /// 次のページが存在しない場合は無視する。
+        /// </summary>
+        private void MoveNext()
+        {
+            if (_messages is null or { Length: 0 })
+            {
+                return;
+            }
+
+            if (_currentIndex + 1 < _messages.Length)
+            {
+                _currentIndex++;
+               
+                _messagePrinter?.ShowMessage(_messages[_currentIndex]);
+                _messagePrinter.ApplyEmphasis(_emphasisTexts);
+            }
+            else
+            {
+                OnSequenceEnd();
+            }
+        }
+
+        private void OnSequenceEnd()
+        {
+            Debug.Log("シーケンス終了");
+        }
+
+        public void ResetMessage()
+        {
+            _currentIndex = -1;
+            MoveNext();
+        }
     }
 }
